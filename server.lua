fb00b6389bca7055e18c0c135b35d227a098ffa9
WirzRexTon <73710374+WirzRexTon@users.noreply.github.com>
2021-05-23 16:57:53 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,6 +1,6 @@
 
 
-local DISCORD_WEBHOOK = "https://discord.com/api/webhooks/812057045133033492/kv82Bl0mElK7JCqrfhUOvT-0kPyJnvrJEyRVtK7igArPKh_eI-eT150yr8j8uPyVEj3s"
+local DISCORD_WEBHOOK = "INSERER VOTRE DISCORD WEBHOOK - INSERT YOUR DISCORD WEBHOOK"
 local DISCORD_NAME = "Centrale LSPD"
 local DISCORD_IMAGE = "https://i.imgur.com/rnMbgUi.png"
 
